a7499a914eaaa80e9d96f4792d4b9615a146c223
MalithaLiyanage <malitharanu82@gmail.com>
2022-03-22 16:36:42 +0530

    Added a draft arena

---
+++ packages/renderer/src/pages/Arena.tsx
@@ -0,0 +1,21 @@
+const Arena = () => {
+    
+    return (
+        <div className="arena-container">
+            <div className="header">
+                <h1>Tank Game</h1>
+            </div>
+            <div className="score-container">
+
+            </div>
+            <div className="play-area-container">
+
+            </div>
+            <div className="footer">
+                <h4>Created by Malitha</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Arena;
